6995a73ab73533df4f14269993b1e32b31d86ccb
Austin Farhoudi <farhoudi@umich.edu>
2023-02-13 18:57:19 -0500

    change name

--- pack.cpp
+++ pack.cpp
@@ -48,41 +48,7 @@ void Pack::reset()
 
 void Pack::shuffle()
 {
-  Card half1[PACK_SIZE / 2]; // holds first half of pack
-  Card half2[PACK_SIZE / 2]; // holds second half of pack
-  const int NUM_SHUFFLES = 7;
-
-  // perform in shuffle 7x
-  for (int i = 0; i < NUM_SHUFFLES; i++)
-  {
-    // split cards
-    for (int j = 0; j < PACK_SIZE / 2; j++)
-    {
-      half1[j] = cards[j];
-    }
-    for (int j = PACK_SIZE / 2; j < PACK_SIZE; j++)
-    {
-      half2[j] = cards[j];
-    }
-
-    // alternate cards
-    int incrementHalf1 = 0;
-    int incrementHalf2 = 0;
-    for (int j = 0; j < PACK_SIZE; j++)
-    {
-      if (j % 2 == 0)
-      {
-        cards[j] = half2[incrementHalf2];
-        incrementHalf2++;
-      }
-      else 
-      {
-        cards[j] = half1[incrementHalf1];
-        incrementHalf1++;
-      }
-    }
-  }
-  next = 0;
+  
 }
 
 bool Pack::empty() const
