054fd5fad1a9133de500957ef157d4f0d9e46cc6
cordymw <123425676+cordymw@users.noreply.github.com>
2023-02-13 22:36:29 -0500

    Update Card_tests.cpp

--- Card_tests.cpp
+++ Card_tests.cpp
@@ -3,6 +3,7 @@
 #include "Card.h"
 #include "unit_test_framework.h"
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
@@ -13,6 +14,35 @@ TEST(test_card_ctor) {
     ASSERT_EQUAL(HEARTS, c.get_suit());
 }
 
-// Add more test cases here
+TEST(faceAce){
+Card c(ACE, HEARTS);
+
+ASSERT_EQUAL(c.is_face_or_ace(), 1);
+
+Card d(EIGHT, SPADES);
+ASSERT_EQUAL(d.is_face_or_ace(), 0);
+}
+
+TEST(right_bower){
+Card c(JACK, HEARTS);
+
+ASSERT_EQUAL(c.is_right_bower(HEARTS), 1);
+
+ASSERT_EQUAL(c.is_right_bower(SPADES), 0);
+}
+
+TEST(left_bower){
+Card c(JACK, HEARTS);
+
+ASSERT_EQUAL(c.is_left_bower(DIAMONDS), 1);
+
+ASSERT_EQUAL(c.is_left_bower(SPADES), 0);
+}
+
+TEST(is_trump){
+Card c(ACE, HEARTS);
+
+ASSERT_EQUAL(c.is_trump(HEARTS), 1);
+}
 
 TEST_MAIN()
