6ac6bd24464acbf5f8a075de7adafbac3b62829e
cordymw <123425676+cordymw@users.noreply.github.com>
2023-02-18 22:01:49 -0500

    Update Player_tests.cpp

--- Player_tests.cpp
+++ Player_tests.cpp
@@ -2,13 +2,6 @@
 
 #include "Player.h"
 #include "unit_test_framework.h"
-
-#include <iostream>
-
-using namespace std;// Project UID 1d9f47bfc76643019cfbf037641defe1
-
-#include "Player.h"
-#include "unit_test_framework.h"
 #include <cassert>
 #include <iostream>
 
@@ -21,124 +14,20 @@ TEST(test_player_get_name) {
     delete alice;
 }
 
-TEST(test_add_card) {
+TEST(test_add_card_and_handsize_and_handat) {
     Player * alice = Player_factory("Alice", "Simple");
     
     Card test(NINE, HEARTS);
-    Simple alice.add_card(test);//why do you not work
+    alice->add_card(test);
+
+    ASSERT_EQUAL(alice->hand_size(), 1);
 
 
-    delete alice;
-}
-
-
-
-
-
-//SIMPLE
-
-
-TEST(test_make_trump) {
-    Player * alice = Player_factory("Alice", "Simple");
-
-    Card one(NINE, HEARTS);
-    Card two(TEN, DIAMONDS);
-    Card three(JACK, SPADES);
-    Card four(QUEEN, CLUBS);
-    Card five(ACE, HEARTS);
-
-    alice.add_card(one);
-    alice.add_card(two);
-    alice.add_card(three);
-    alice.add_card(four);
-    alice.add_card(five);
-
-    Card upcard(TEN, SPADES);
-    bool is_dealer = 0;
-    int round = 1;
-    Suit order_up;
-
-    bool real = alice.make_trump(upcard, is_dealer, round, order_up);
-
-    assert(real == 1);
+    Card pls = alice->hand_at(0);
+    ASSERT_EQUAL(pls, test);
 
     delete alice;
 }
-
-TEST(test_player_get_name) {
-    Player * alice = Player_factory("Alice", "Simple");
-    ASSERT_EQUAL("Alice", alice->get_name());
-
-    delete alice;
-}
-
-TEST(test_player_get_name) {
-    Player * alice = Player_factory("Alice", "Simple");
-    ASSERT_EQUAL("Alice", alice->get_name());
-
-    delete alice;
-}
-
-TEST(test_player_get_name) {
-    Player * alice = Player_factory("Alice", "Simple");
-    ASSERT_EQUAL("Alice", alice->get_name());
-
-    delete alice;
-}
-
-
-
-
-//HUMAN
-
-TEST(test_player_get_name) {
-    Player * alice = Player_factory("Alice", "Simple");
-    ASSERT_EQUAL("Alice", alice->get_name());
-
-    delete alice;
-}
-
-TEST(test_player_get_name) {
-    Player * alice = Player_factory("Alice", "Simple");
-    ASSERT_EQUAL("Alice", alice->get_name());
-
-    delete alice;
-}
-
-TEST(test_player_get_name) {
-    Player * alice = Player_factory("Alice", "Simple");
-    ASSERT_EQUAL("Alice", alice->get_name());
-
-    delete alice;
-}
-
-TEST(test_player_get_name) {// Project UID 1d9f47bfc76643019cfbf037641defe1
-
-#include "Player.h"
-#include "unit_test_framework.h"
-#include <cassert>
-#include <iostream>
-
-using namespace std;
-
-TEST(test_player_get_name) {
-    Player * alice = Player_factory("Alice", "Simple");
-    ASSERT_EQUAL("Alice", alice->get_name());
-
-    delete alice;
-}
-
-TEST(test_add_card) {
-    Player * alice = Player_factory("Alice", "Simple");
-    
-    Card test(NINE, HEARTS);
-    Simple alice.add_card(test);//why do you not work
-
-
-    delete alice;
-}
-
-
 
 
 
@@ -154,18 +43,18 @@ TEST(test_make_trump_simple) {
     Card four(QUEEN, CLUBS);
     Card five(ACE, HEARTS);
 
-    alice.add_card(one);
-    alice.add_card(two);
-    alice.add_card(three);
-    alice.add_card(four);
-    alice.add_card(five);
+    alice->add_card(one);
+    alice->add_card(two);
+    alice->add_card(three);
+    alice->add_card(four);
+    alice->add_card(five);
 
     Card upcard(TEN, SPADES);
     bool is_dealer = 0;
     int round = 1;
     Suit order_up;
 
-    bool real = alice.make_trump(upcard, is_dealer, round, order_up);
+    bool real = alice->make_trump(upcard, is_dealer, round, order_up);
 
     assert(real == 1);
 
@@ -174,21 +63,80 @@ TEST(test_make_trump_simple) {
 
 TEST(test_add_discard_simple) {
     Player * alice = Player_factory("Alice", "Simple");
-    ASSERT_EQUAL("Alice", alice->get_name());
+    
+    Card one(NINE, HEARTS);
+    Card two(TEN, DIAMONDS);
+    Card three(JACK, SPADES);
+    Card four(QUEEN, CLUBS);
+    Card five(ACE, HEARTS);
+
+    alice->add_card(one);
+    alice->add_card(two);
+    alice->add_card(three);
+    alice->add_card(four);
+    alice->add_card(five);
+
+    Card upcard(TEN, SPADES);
+
+    alice->add_and_discard(upcard);
+
+    for(int i= 0; i<5; ++i){
+
+        Card test = alice->hand_at(i);
+
+        ASSERT_NOT_EQUAL(test, one);
+
+    }
 
     delete alice;
 }
 
 TEST(test_lead_card_simple) {
     Player * alice = Player_factory("Alice", "Simple");
-    ASSERT_EQUAL("Alice", alice->get_name());
+    
+    
+    Card one(NINE, HEARTS);
+    Card two(TEN, DIAMONDS);
+    Card three(JACK, SPADES);
+    Card four(QUEEN, CLUBS);
+    Card five(ACE, HEARTS);
+
+    alice->add_card(one);
+    alice->add_card(two);
+    alice->add_card(three);
+    alice->add_card(four);
+    alice->add_card(five);
+
+    Card test= alice->lead_card(HEARTS);
+
+    ASSERT_EQUAL(test, four);
+
 
     delete alice;
 }
 
 TEST(test_play_card_simple) {
     Player * alice = Player_factory("Alice", "Simple");
-    ASSERT_EQUAL("Alice", alice->get_name());
+    
+    
+    Card one(NINE, HEARTS);
+    Card two(TEN, DIAMONDS);
+    Card three(JACK, SPADES);
+    Card four(QUEEN, CLUBS);
+    Card five(ACE, HEARTS);
+
+    alice->add_card(one);
+    alice->add_card(two);
+    alice->add_card(three);
+    alice->add_card(four);
+    alice->add_card(five);
+
+    Card led(QUEEN, SPADES);
+
+    Card test = alice->play_card(led, HEARTS);
+
+    ASSERT_EQUAL(test, three);
+
 
     delete alice;
 }
@@ -200,54 +148,44 @@ TEST(test_play_card_simple) {
 
 TEST(test_make_trump_human) {
     Player * alice = Player_factory("Alice", "Simple");
-    ASSERT_EQUAL("Alice", alice->get_name());
+    
+    
+
+
 
     delete alice;
 }
 
 TEST(test_add_discard_human) {
     Player * alice = Player_factory("Alice", "Simple");
-    ASSERT_EQUAL("Alice", alice->get_name());
+    
+    
+
+
 
     delete alice;
 }
 
 TEST(test_lead_card_human) {
     Player * alice = Player_factory("Alice", "Simple");
-    ASSERT_EQUAL("Alice", alice->get_name());
+    
+    
+
+
 
     delete alice;
 }
 
 TEST(test_play_card_human) {
     Player * alice = Player_factory("Alice", "Simple");
-    ASSERT_EQUAL("Alice", alice->get_name());
+    
+    
+
+
 
     delete alice;
 }
 
 
-
-TEST_MAIN()
-
-    Player * alice = Player_factory("Alice", "Simple");
-    ASSERT_EQUAL("Alice", alice->get_name());
-
-    delete alice;
-}
-
-
-
-TEST_MAIN()
-
-
-TEST(test_player_get_name) {
-    Player * alice = Player_factory("Alice", "Simple");
-    ASSERT_EQUAL("Alice", alice->get_name());
-
-    delete alice;
-}
-
-// Add more tests here
 
 TEST_MAIN()
